0e65688eb4d444b3ab26eef3310e0a038ec3ccf4
Gregory Tandiono <gregtandiono@gmail.com>
2019-08-23 00:01:09 +0800

    WIP org service

--- src/models/comment.js
+++ src/models/comment.js
@@ -14,6 +14,10 @@ const commentSchema = new Schema({
   orgId: {
     type: Schema.Types.ObjectId,
     required: true
+  },
+  deleted: {
+    type: Boolean,
+    default: false
   }
 }, { timestamps: true })
 
---
+++ src/services/orgs.js
@@ -0,0 +1,21 @@
+const Org = require('../models/org')
+
+class Orgs {
+  constructor () {
+    this.org = new Org()
+  }
+
+  async listComments (req, res) {
+  }
+
+  async createComment (req, res) {
+  }
+
+  async removeComments (req, res) {
+  }
+
+  async listMembers (req, res) {
+  }
+}
+
+module.exports = Orgs
