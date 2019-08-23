97f54f869853e1529069a1dcba27637cb1f93317
Gregory Tandiono <gregtandiono@gmail.com>
2019-08-24 03:10:28 +0800

    Disable morgan for unit test

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.use(cors())
 app.use(helmet())
 app.use(compression())
 app.use(express.json())
-app.use(morgan('tiny'))
+
+if (process.env.NODE_ENV !== 'test') app.use(morgan('tiny'))
 app.use(routes)
 
 app.listen(config.port, () => console.log(`
