812850c271ad88a2cf209fc6b174461f20253fca
Gregory Tandiono <gregtandiono@gmail.com>
2019-08-24 13:47:26 +0800

    Conditionally silence winston logger during unit testing.

--- src/services/members.js
+++ src/services/members.js
@@ -1,15 +1,17 @@
 const utils = require('./utils')
+const logger = require('../logger')
 const Member = require('../models/member')
 
 class Members {
   async list (req, res) {
     try {
       await utils.validateOrg(req)
-
       const members = await Member.find({ org: req.params.orgName }).sort({ followers: -1 })
       res.status(200).json(members)
     } catch (error) {
-      res.status(400).send({ message: error.toString() })
+      const errStr = utils.errorStringifier(error)
+      logger.error(`Members.list ${errStr}`)
+      res.status(400).send({ message: errStr })
     }
   }
 }
